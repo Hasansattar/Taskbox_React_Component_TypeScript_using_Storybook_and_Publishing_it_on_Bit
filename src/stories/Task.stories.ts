f6a590e5c3db89481731d93bcb06dfd40ab53971
hasansattar <hasansattar650@gmail.com>
2020-11-22 19:59:01 +0500

    Taskbox React Component TypeScript using storybook

---
+++ src/stories/Task.stories.ts
@@ -0,0 +1,14 @@
+import React from 'react'
+import Task from '../components/Input'
+
+export default {
+    title: "Task",
+    component: Task
+}
+
+interface TaskProps {
+    task: {
+        id: string;
+        description: string;
+    }
+}
